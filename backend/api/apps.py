851eb24a71bbe6ac3841b41645f2c2f5f2354f54
lhy2016 <lhy920104@gmail.com>
2021-11-09 17:12:27 -0800

    initial commit
    added models for user, vehicle, and rental

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AvcloudConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'avCloud'
+    name = 'api'
