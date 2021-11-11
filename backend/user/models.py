77919d89b8dac155701898773ff7d6c2c063e2a4
lhy2016 <lhy920104@gmail.com>
2021-11-11 12:16:56 -0800

    update api

--- backend/user/models.py
+++ backend/user/models.py
@@ -3,7 +3,7 @@ from django.utils.timezone import now
 # Create your models here.
 class User(models.Model):
     username = models.CharField(max_length=50, unique=True)
-    password = models.CharField(max_length=50)
+    password = models.CharField(max_length=500)
     create_on = models.DateTimeField(default=now)
     is_deleted = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
