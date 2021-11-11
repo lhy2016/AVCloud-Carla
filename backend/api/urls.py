7dff5e503dc80b5186a92c08ceac5b0aeee6a47b
Inderjit Bassi <inderjit.bassi@sjsu.edu>
2021-11-10 22:11:43 -0800

    Created vehicle add api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,9 +7,11 @@ urlpatterns = [
     #^$ means this is the default homepage page of the section..
     #/homepage/
     path('', views.homepage, name="homepage"),
-    path('add', views.add_vehicle, name="add_vehicle"),
+    path('add_vehicle/<str:make>/<str:model>/<str:year>/<str:color>/<str:created_on>/<str:is_deleted>', views.add_vehicle, name="add_vehicle"),
     path('remove', views.remove_vehicle, name="remove_vehicle"),
     path('markAvailable', views.mark_available, name="mark_available"),
     path('markUnavailable', views.mark_unavailable, name="mark_unavailable"),
+    path('rent_vehicle', views.rent_vehicle, name="rent_vehicle"),
+    path('return_vehicle', views.return_vehicle, name="return_vehicle")
 
 ]
